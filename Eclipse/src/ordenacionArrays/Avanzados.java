1369d29d7db3f2c1dbc2e06b041af0ba37b3b1e5
PJarana <pablojosejarana21@gmail.com>
2017-03-20 12:54:06 +0100

    A tope con el método Shell

--- Eclipse/src/ordenacionArrays/Avanzados.java
+++ Eclipse/src/ordenacionArrays/Avanzados.java
@@ -2,21 +2,36 @@ package ordenacionArrays;
 
 public class Avanzados {
 	
-	//A continuación vemos el método de ordenación de Inserción para entender mejor el Shell, ya que éste es una versión mejorada de Inserción
-	public static int[] metodoInsercion(int[]array, int tamanyo)
+	//i se inicializa a 1(2da posicion del array), j se inicializa a i y va avanzando hasta la posicion 1 intercambiando los elementos que sean menores
+	public static void metodoInsercion(int[]array)
 	{
 		int i, j, aux;
-		for(i=1;i<tamanyo;i++)
+		
+		for(i=1;i<array.length;i++)
 		{
-			for(j=i;array[j-1]>array[j]&&j>0;j--)
+			for(j=i;j>0&&array[j-1]>array[j];j--)
 			{
 				aux = array[j]; 
 				array[j] = array[j-1];
 				array[j-1] = aux;
 			}
 		}
-		return array;
 	}
-	
-
+	public static void shell(int []A){
+		   int salto, aux, i;
+		   boolean cambios;
+		   for(salto=A.length/2; salto!=0; salto/=2){
+		           cambios=true;
+		           while(cambios){ // Mientras se intercambie algún elemento
+		                       cambios=false;
+		                       for(i=salto; i< A.length; i++) // se da una pasada
+		                               if(A[i-salto]>A[i]){ // y si están desordenados
+		                                     aux=A[i]; // se reordenan
+		                                     A[i]=A[i-salto];
+		                                     A[i-salto]=aux;
+		                                     cambios=true; // y se marca como cambio.
+		                               }
+		                        }
+		            }
+		}
 }
--- Eclipse/src/ordenacionArrays/AvanzadosConductores.java
+++ Eclipse/src/ordenacionArrays/AvanzadosConductores.java
@@ -5,13 +5,12 @@ public class AvanzadosConductores {
 	{
 	
 		int i;
-		int tamanyo=12;
 		int[]array={7,4,23,18,12,32,14,23,64,14,10,95};
-		int[]arrayOrdenado=Avanzados.metodoInsercion(array, tamanyo);
+		Avanzados.shell(array);
 		
-		for(i=0;i<tamanyo;i++)
+		for(i=0;i<array.length;i++)
 		{
-			System.out.println(arrayOrdenado[i]);
+			System.out.println(array[i]);
 		}
 	}
 }
