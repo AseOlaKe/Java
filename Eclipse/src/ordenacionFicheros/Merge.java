70fa980e42a6d4335a6e35e1f99cc7abe4e7e294
AseOlaKe <pablojosejarana21@gmail.com>
2017-04-17 17:27:53 +0200

    Método Merge para ordenar enteros en ficheros hecho!

--- Eclipse/src/ordenacionFicheros/Merge.java
+++ Eclipse/src/ordenacionFicheros/Merge.java
@@ -10,30 +10,52 @@ public class Merge {
 				   String AUX2  =  ".\\src\\ordenacionFicheros\\aux2.txt" ;
 			     try {
 			       boolean sorted = split(INPUT, AUX1, AUX2);
-			       while (!sorted) {
+			       while (!sorted) { //Mientras no estén ordenados, divide y vencerás
 			         merge(AUX1, AUX2, INPUT);
 			         sorted = split(INPUT, AUX1, AUX2);
 			       }
-			       System.out.println("Ya esta chavale ya esta ordenao del to, o no ");
+			       System.out.println("Ya esta chavale ya esta ordenao del to, o no ");//Ea con dio
 			     } catch (IOException ex) {
 			       System.out.println("No se pudo ordenar el fichero: "+ex);
 			     }
 			   }
+			   /*
+			    * Interfaz
+			    * 
+			    * Este método divide un fichero en otros dos, colocando en éstos los números en secuencias ordenadas
+			    * prototipo: boolean split(String input,String output1,String output2)
+			    * Precondiciones: Las cadenas no pueden estar vacías
+			    * Entrada: 3 cadenas
+			    * Salidas: Un valor booleano
+			    * Postcondiciones: True si el fichero está ordenado y false si no
+			    * 
+			    * RESGUARDO
+			    * 
+			    * public boolean split(String input,String output1,String output2)
+			    * {
+			    * 	return false;
+			    * }
+			    */
 			
 			    public boolean split(String input,String output1,String output2) throws IOException {
 			
-			     BufferedReader in = new BufferedReader(new FileReader(input));
+			     BufferedReader in = new BufferedReader(new FileReader(input)); 
 			     BufferedWriter out = new BufferedWriter(new FileWriter(output1));
 			     BufferedWriter other = new BufferedWriter(new FileWriter(output2));
 		
 			     boolean sorted = true;
-			     String previous = ""  ;
+			     String previous = "";
 			     String current = in.readLine();
-			
-			     while (current != null) {
-			       if (previous.compareTo(current) > 0) {
+			     out.write(current);
+			     out.newLine();
+			     previous = current;
+			     current = in.readLine();
+			     while (current != null) { //Paso las cadenas a enteros ya que el compareTo de String compara solo el primer carácter, entonces si escribo "20" y "3" me dirá que 20 es menor que 3
+			    	 int previousNumber=Integer.parseInt(previous);
+				     int currentNumber=Integer.parseInt(current);
+			       if (previousNumber>currentNumber) {
 			         sorted = false;
-			        BufferedWriter tmp = out;
+			        BufferedWriter tmp = out;//Si el número previo es mayor al actual, significa que no está ordenado y escribimos en el otro fichero
 			         out = other;
 			         other = tmp;
 			       }
@@ -60,8 +82,10 @@ public class Merge {
 			     String current1 = in1.readLine();
 			     String current2 = in2.readLine();
 			
-			     while (current1 != null && current2 != null) {
-			    	        if (current1.compareTo(current2) <= 0) {
+			     while (current1 != null && current2 != null) { //Vamos escribiendo en el fichero original en orden
+			    	 int currentNumber1=Integer.parseInt(current1);
+			    	 int currentNumber2=Integer.parseInt(current2);
+			    	        if (currentNumber1<=currentNumber2) {
 			         out.write(current1);
 			         out.newLine();
 			         current1 = in1.readLine();
@@ -83,5 +107,8 @@ public class Merge {
 			       out.newLine();
 			       current2 = in2.readLine();
 			     }
+			     in1.close();
+			     in2.close();
+			     out.close();
 			   }
 }
